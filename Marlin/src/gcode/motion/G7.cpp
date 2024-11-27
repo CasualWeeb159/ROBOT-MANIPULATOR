a797bbf64ac6cd02093b556c50d3656556be33c7
Ondřej Chalupa <chaluon3@student.cvut.cz>
2024-11-27 22:40:06 +0100

    oprava

--- Marlin/src/gcode/motion/G7.cpp
+++ Marlin/src/gcode/motion/G7.cpp
@@ -1,9 +1,13 @@
 #include "../gcode.h"
 #include "../../module/motion.h"
 
-abc_pos_t robot_angles = {300,300,300};
+abc_pos_t robot_angles;
 void GcodeSuite::G7() {
-    
+    inverse_kinematics(current_position);
+    robot_angles.a = delta.a;
+    robot_angles.b = delta.b;
+    robot_angles.c = delta.c;
+
     if (parser.seenval('A')) robot_angles.a = parser.value_float();
     if (parser.seenval('B')) robot_angles.b = parser.value_float();
     if (parser.seenval('C')) robot_angles.c = parser.value_float();
